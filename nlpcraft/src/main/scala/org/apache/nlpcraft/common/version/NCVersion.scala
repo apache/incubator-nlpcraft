9f973b44df60b37ceb33f6bc570baee3c250a296
Aaron Radzinski <aradzinski@datalingvo.com>
2020-12-31 14:29:56 -0800

    Fixed release date for 0.7.3 release.

--- nlpcraft/src/main/scala/org/apache/nlpcraft/common/version/NCVersion.scala
+++ nlpcraft/src/main/scala/org/apache/nlpcraft/common/version/NCVersion.scala
@@ -53,7 +53,7 @@ object NCVersion extends LazyLogging {
         Version("0.7.0", LocalDate.of(2020, 9, 29)),
         Version("0.7.1", LocalDate.of(2020, 10, 29)),
         Version("0.7.2", LocalDate.of(2020, 11, 19)),
-        Version("0.7.3", LocalDate.of(2020, 12, 19))
+        Version("0.7.3", LocalDate.of(2020, 12, 31))
     ).sortBy(_.version)
     // +=================================================+
     // | UPDATE THIS SEQUENCE FOR EACH RELEASE MANUALLY. |
