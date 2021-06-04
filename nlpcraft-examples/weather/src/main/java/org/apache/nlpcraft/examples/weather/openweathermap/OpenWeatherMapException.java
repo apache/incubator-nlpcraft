e5d50bfe3b9cfbcefad325a2c4739dd63fafd33b
Rahul Padmanabhan <rahul3@mail.usf.edu>
2021-06-03 21:10:35 -0400

    fixed package name and directory name

--- nlpcraft-examples/weather/src/main/java/org/apache/nlpcraft/examples/weather/openweathermap/OpenWeatherMapException.java
+++ nlpcraft-examples/weather/src/main/java/org/apache/nlpcraft/examples/weather/openweathermap/OpenWeatherMapException.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.nlpcraft.examples.weather.darksky;
+package org.apache.nlpcraft.examples.weather.openweathermap;
 
 /**
  * OpenWeatherMapException exception.
