f29666728879d384a9c527c09aa68f52dbf14342
unknown <aradzinski@datalingvo.com>
2021-06-11 17:57:01 -0700

    Open Census support refactoring.

--- nlpcraft/src/main/scala/org/apache/nlpcraft/model/opencensus/stats/NCOpenCensusModelStats.scala
+++ nlpcraft/src/main/scala/org/apache/nlpcraft/model/opencensus/stats/NCOpenCensusModelStats.scala
@@ -17,71 +17,25 @@
 
 package org.apache.nlpcraft.model.opencensus.stats
 
-import java.util
-import java.util.Collections
-
-import io.opencensus.stats.Aggregation.{Count, Distribution}
-import io.opencensus.stats.Measure.{MeasureDouble, MeasureLong}
-import io.opencensus.stats.View.Name
+import io.opencensus.stats.Measure._
 import io.opencensus.stats._
+import org.apache.nlpcraft.common.opencensus.NCOpenCensusStats
 
 /**
   * OpenCensus stats instrumentation.
   */
-trait NCOpenCensusModelStats {
+trait NCOpenCensusModelStats extends NCOpenCensusStats {
     val M_SYS_LATENCY_MS: MeasureLong =
         MeasureLong.create("sys_latency", "The latency of system tasks", "ms")
     val M_USER_LATENCY_MS: MeasureLong =
         MeasureLong.create("user_latency", "The latency of user tasks", "ms")
-    
-    /**
-      * Records OpenCensus metrics.
-      *
-      * @param pairs Pairs of OC measure and its value. Values must be `Long` or `Double` only.
-      */
-    def recordStats(pairs: (Measure, AnyVal)*): Unit = {
-        val map = Stats.getStatsRecorder.newMeasureMap()
-        
-        for ((m, v) <- pairs) {
-            m match {
-                case d: MeasureDouble => map.put(d, v.asInstanceOf[Double])
-                case l: MeasureLong => map.put(l, v.asInstanceOf[Long])
-                case _ => throw new AssertionError()
-            }
-        }
-        
-        map.record()
-    }
-    
+
     init()
     
     /**
       *
       */
     private def init(): Unit = {
-        val restLatDist = Distribution.create(BucketBoundaries.create(util.Arrays.asList(
-            0.0, 25.0, 100.0, 200.0, 400.0, 800.0, 10000.0
-        )))
-        
-        def mkViews(m: Measure, name: String): List[View] = {
-            List(
-                View.create(
-                    Name.create(s"$name/latdist"),
-                    s"The distribution of the '$name' latency",
-                    m,
-                    restLatDist,
-                    Collections.emptyList()
-                ),
-                View.create(
-                    Name.create(s"$name/count"),
-                    s"The number of the '$name' invocations",
-                    m,
-                    Count.create,
-                    Collections.emptyList()
-                )
-            )
-        }
-        
         val views = List(
             mkViews(M_SYS_LATENCY_MS, "sys_task"),
             mkViews(M_USER_LATENCY_MS, "user_task")
