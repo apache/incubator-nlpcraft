6282afd331bfcdaa31260f8339ab5a07f9eb3187
Aaron Radzinski <aradzinski@datalingvo.com>
2021-12-07 10:06:26 -0800

    WIP

--- nlpcraft/src/main/scala/org/apache/nlpcraft/NCModelConfig.java
+++ nlpcraft/src/main/scala/org/apache/nlpcraft/NCModelConfig.java
@@ -15,18 +15,9 @@
  * limitations under the License.
  */
 
-package org.apache.nlpcraft;/*
-   _________            ______________
-   __  ____/_______________  __ \__  /_____ _____  __
-   _  /    _  __ \_  ___/_  /_/ /_  /_  __ `/_  / / /
-   / /___  / /_/ /(__  )_  ____/_  / / /_/ /_  /_/ /
-   \____/  \____//____/ /_/     /_/  \__,_/ _\__, /
-                                            /____/
+package org.apache.nlpcraft;
 
-          2D ASCII JVM GAME ENGINE FOR SCALA3
-              (C) 2021 Rowan Games, Inc.
-                ALl rights reserved.
-*/
+import java.util.*;
 
 /**
  *
@@ -77,7 +68,7 @@ public interface NCModelConfig {
      *
      * @return
      */
-    NCEntityParser getEntityParser();
+    List<NCEntityParser> getEntityParsers();
 
     /**
      * Gets unique, <i>immutable</i> ID of this model.
